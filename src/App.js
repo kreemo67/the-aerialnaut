463a498ecb69a13d74f50d1c30fd62cb72e2af67
Kareem Abdo <kreemo67@gmail.com>
2022-12-17 17:20:07 -0600

    end sat work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+/** let custom components start with a capital letter */
+import Todo from "./components/Todo";
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1> My todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Created arialnaut.com" />
+      <Todo text="Make it better than wix" />
+    </div>
+  );
 }
 
 export default App;
